01565c3da0adad8adbd7fb4acd55ca3d83e0b72a
aleksandrandreichenko <andreichenko@web.de>
2021-12-15 20:39:53 +0300

    iam s3 role

--- aws/iam.tf
+++ aws/iam.tf
@@ -20,3 +20,38 @@ resource "aws_iam_role" "bastion_role" {
 EOF
 
 }
+
+resource "aws_iam_role_policy" "bastion_s3" {
+  name = "bastion-s3"
+  role = aws_iam_role.bastion_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+    "Sid": "AllowBucketListing",
+    "Action": [
+      "s3:ListBucket"
+    ],
+    "Resource": [
+      "${var.arn_prefix}:s3:::${local.infrastructure_bucket.id}"
+    ],
+    "Effect": "Allow"
+  },
+  {
+    "Sid": "LimitAccessOnlyToSubKey",
+    "Action": [
+      "s3:PutObject",
+      "s3:GetObject"
+    ],
+    "Resource": [
+      "${var.arn_prefix}:s3:::${local.infrastructure_bucket.id}/${var.infrastructure_bucket_bastion_key}/*"
+    ],
+    "Effect": "Allow"
+  }
+  ]
+}
+EOF
+
+}
