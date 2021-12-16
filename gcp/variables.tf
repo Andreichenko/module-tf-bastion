3c2fd3a174286d542847d24fcdfe201a959b153a
aleksandrandreichenko <andreichenko@web.de>
2021-12-16 21:16:50 +0300

    gcp variables

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -20,3 +20,14 @@ variable "infrastructure_bucket_bastion_key" {
   description = "The key; sub-directory in $infrastructure_bucket where the bastion will be allowed to read and write. Do not specify a trailing slash. This allows sharing a GCS bucket among multiple invocations of this module."
   default     = "bastion"
 }
+
+variable "unattended_upgrade_reboot_time" {
+  description = "The time that the bastion should reboot, when necessary, after an an unattended upgrade. This sets the option in /etc/apt/apt.conf.d/50unattended-upgrades"
+
+  # By default the time zone is UTC.
+  default = "22:30"
+}
+
+variable "unattended_upgrade_email_recipient" {
+  description = "An email address where unattended upgrade errors should be emailed. This sets the option in /etc/apt/apt.conf.d/50unattended-upgrades"
+}
