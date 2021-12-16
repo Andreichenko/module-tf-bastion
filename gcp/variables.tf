c875a36624c1f1027a47348aa87de9e2549e7827
aleksandrandreichenko <andreichenko@web.de>
2021-12-16 21:16:02 +0300

    gcp variables

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -6,3 +6,17 @@ variable "bastion_name" {
 variable "region" {
   description = "The region where the bastion should be provisioned. This is a required input for the google_compute_region_instance_group_manager Terraform resource, and is not inherited from the provider."
 }
+
+variable "availability_zones" {
+  description = "The availability zones within $region where the Auto Scaling Group can place the bastion."
+  type        = list
+}
+
+variable "infrastructure_bucket" {
+  description = "An GCS bucket to store data that should persist on the bastion when it is recycled by the Auto Scaling Group, such as SSH host keys. This can be set in the environment via `TF_VAR_infrastructure_bucket`"
+}
+
+variable "infrastructure_bucket_bastion_key" {
+  description = "The key; sub-directory in $infrastructure_bucket where the bastion will be allowed to read and write. Do not specify a trailing slash. This allows sharing a GCS bucket among multiple invocations of this module."
+  default     = "bastion"
+}
