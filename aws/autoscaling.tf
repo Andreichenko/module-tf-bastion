67d1741cdfcc02749a0696970dfaf7d8d4cb65ae
aleksandrandreichenko <andreichenko@web.de>
2021-12-21 12:02:35 +0300

    bastion aws configuration

--- aws/autoscaling.tf
+++ aws/autoscaling.tf
@@ -1,4 +1,14 @@
 # The Auto Scaling Group increases the availability of the bastion by
 # replacing an unhealthy EC2 instance or recovering from an
 # availability zone failure.
-resource "aws_autoscaling_group" "bastion" {}
+resource "aws_autoscaling_group" "bastion" {
+    # The Launch Configuration ID is part of the AUto Scalign Group name,
+# to force the ASG and its EC2 to be recreated.
+  name = "asg-${aws_launch_configuration.bastion.id}"
+  launch_configuration = aws_launch_configuration.bastion.name
+  min_size            = 1
+  max_size            = 1
+  desired_capacity    = 1
+  vpc_zone_identifier = flatten(var.vpc_subnet_ids)
+
+}
