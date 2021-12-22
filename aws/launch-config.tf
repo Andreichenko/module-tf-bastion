1e41a11348f205cd0c2e65764cdaa0c7df78425a
aleksandrandreichenko <andreichenko@web.de>
2021-12-22 13:51:45 +0300

    added template and lc

---
+++ aws/launch-config.tf
@@ -0,0 +1,22 @@
+data "template_file" "bastion_user_data" {
+  template = file("${path.module}/bastion-userdata.tmpl")
+
+  vars = {
+    bastion_name                      = var.bastion_name
+    infrastructure_bucket             = local.infrastructure_bucket.id
+    infrastructure_bucket_bastion_key = var.infrastructure_bucket_bastion_key
+    # THe ROute53 zone to add a `bastion` A record.
+    zone_id = var.route53_zone_id
+    # Configuration options for unattended upgrades, added to /etc/apt/apt.conf.d/50unattended-upgrades
+    unattended_upgrade_reboot_time        = var.unattended_upgrade_reboot_time
+    unattended_upgrade_email_recipient    = var.unattended_upgrade_email_recipient
+    unattended_upgrade_additional_configs = var.unattended_upgrade_additional_configs
+    remove_root_access                    = var.remove_root_access
+    additional_user_data                  = var.additional_user_data
+    # Join the rendered templates per additional user into a single string variable.
+
+    additional_user_templates                                   = join("\n", data.template_file.additional_user.*.rendered)
+    infrastructure_bucket_additional_external_users_script_etag = aws_s3_bucket_object.additional-external-users-script.etag
+    additional-external-users-script-md5                        = local.additional-external-users-script-md5
+  }
+}
