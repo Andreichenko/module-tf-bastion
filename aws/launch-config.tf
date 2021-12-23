78dde9cb06d923b5da3bedb47eb8d424ad3d2c6c
aleksandrandreichenko <andreichenko@web.de>
2021-12-23 12:12:18 +0300

    added launch temp

--- aws/launch-config.tf
+++ aws/launch-config.tf
@@ -22,15 +22,25 @@ data "template_file" "bastion_user_data" {
 }
 
 resource "aws_launch_configuration" "bastion" {
-   name_prefix = "${var.bastion_name}-"
-   image_id      = data.aws_ami.ubuntu.id
+# Generate a unique name for the Launch Configuration,
+# so the Auto Scaling Group can be updated without conflict before destroying the previous Launch Configuration.
+# Also see the related lifecycle block below.
+  name_prefix = "${var.bastion_name}-"
+  image_id      = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-
   iam_instance_profile        = aws_iam_instance_profile.bastion.name
   security_groups             = [aws_security_group.bastion_ssh.id]
   associate_public_ip_address = "true"
-
-  root_block_device {
+  user_data_base64 = base64gzip(data.template_file.bastion_user_data.rendered)
+  key_name         = aws_key_pair.bastion.id
+root_block_device {
     encrypted = var.encrypt_root_volume
+  }
+lifecycle {
+    create_before_destroy = true
+# DO not recreate the Launch Configuration if a newer AMI becomes available.
+# `terrform taint` the Launch Configuration resource to force it to be recreated.
+# In the future we may want to also include user-data in this list.
+    ignore_changes = [image_id]
   }
 }
