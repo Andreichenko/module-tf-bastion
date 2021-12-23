1e115015c3ee913e27669595016766dbef7561eb
aleksandrandreichenko <andreichenko@web.de>
2021-12-23 12:13:15 +0300

    added ami

---
+++ aws/ami.tf
@@ -0,0 +1,12 @@
+# This gets the latest AMI for Ubuntu 18.04
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  # This is Canonical
+  owners = [var.arn_prefix == "arn:aws" ? var.ami_owner_id : var.ami_owner_id_govcloud]
+
+  filter {
+    name   = "name"
+    values = [var.ami_filter_value]
+  }
+}
