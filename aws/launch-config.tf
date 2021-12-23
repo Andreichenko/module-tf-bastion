8de6aee5f7b1a583a6f803eb92410d14eee137ea
aleksandrandreichenko <andreichenko@web.de>
2021-12-23 12:11:55 +0300

    added launch temp

--- aws/launch-config.tf
+++ aws/launch-config.tf
@@ -29,4 +29,8 @@ resource "aws_launch_configuration" "bastion" {
   iam_instance_profile        = aws_iam_instance_profile.bastion.name
   security_groups             = [aws_security_group.bastion_ssh.id]
   associate_public_ip_address = "true"
+
+  root_block_device {
+    encrypted = var.encrypt_root_volume
+  }
 }
