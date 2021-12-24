198dc39c4694a289abb9bd5952a6db7c23fc032a
aleksandrandreichenko <andreichenko@web.de>
2021-12-24 16:55:24 +0300

    external user added

--- aws/external-user.tf
+++ aws/external-user.tf
@@ -2,3 +2,10 @@ locals {
   additional-external-users-script-content = format("%s%s", "#!/bin/bash \n\n", join("\n", data.template_file.additional_external_user.*.rendered))
   additional-external-users-script-md5     = md5(local.additional-external-users-script-content)
 }
+
+resource "aws_s3_bucket_object" "additional-external-users-script" {
+  bucket  = local.infrastructure_bucket.id
+  key     = "${var.infrastructure_bucket_bastion_key}/additional-external-users"
+  content = local.additional-external-users-script-content
+  etag    = md5(local.additional-external-users-script-content)
+}
