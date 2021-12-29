477af23dbe62a99e4b9692023c57eb4dacef5e63
aleksandrandreichenko <andreichenko@web.de>
2021-12-29 13:15:03 +0300

    added script

--- gcp/bastion-startup-script.sh
+++ gcp/bastion-startup-script.sh
@@ -14,20 +14,3 @@ info Exiting...
 exit 0
 fi
 touch /metadata_startup_script-has_run
-
-info Disabling GCE managed Linux users and SSH access.
-cat <<EOT >/etc/default/instance_configs.cfg.template
-[Daemons]
-accounts_daemon = false
-EOT
-/usr/bin/google_instance_setup
-# The above does not stop the google-accounts service immediately.
-systemctl stop google-accounts-daemon.service
-systemctl disable google-accounts-daemon.service
-
-
-
-rra=$(echo ${remove_root_access} |tr '[:upper:]' '[:lower:]')
-systemctl daemon-reload
-systemctl enable additional-external-users
-systemctl start additional-external-users
