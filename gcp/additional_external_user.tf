55dda0d868aa7f152ea7baebf83f03df6ae9b1d3
aleksandrandreichenko <andreichenko@web.de>
2022-01-07 14:03:46 +0300

    fix

--- gcp/additional_external_user.tf
+++ gcp/additional_external_user.tf
@@ -2,3 +2,10 @@ locals {
   additional-external-users-script-content = format("%s%s", "#!/bin/bash \n\n", join("\n", data.template_file.additional_external_user.*.rendered))
   additional-external-users-script-md5     = md5(local.additional-external-users-script-content)
 }
+
+resource "google_storage_bucket_object" "additional-external-users-script" {
+  count   = length(var.additional_external_users) > 0 ? 1 : 0
+  bucket  = var.infrastructure_bucket
+  name    = "${var.infrastructure_bucket_bastion_key}/additional-external-users"
+  content = local.additional-external-users-script-content
+}
