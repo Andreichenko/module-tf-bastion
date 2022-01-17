a59f3116d935b695b58d72f8f7a32d1840a7bd36
aleksandrandreichenko <andreichenko@web.de>
2022-01-17 17:01:28 +0300

    additional user for bastion

--- gcp/additional_external_user.tf
+++ gcp/additional_external_user.tf
@@ -13,6 +13,27 @@ data "template_file" "additional_external_user" {
     user_supplemental_groups = lookup(var.additional_external_users[count.index], "supplemental_groups", "")
     user_authorized_keys     = lookup(var.additional_external_users[count.index], "authorized_keys")
   }
+  template = <<EOF
+function info {
+  echo "user-data: $@"
+}
+info "Creating user:"
+# The printf string is put in single-quotes because it may contain it's own double-quotes.
+printf '  Login: \"$${user_login}\"\n'
+# The ssh_keys variable is put in single-quotes because it may contain it's own double-quotes.
+if [ '$${user_authorized_keys}x' == "x" ]; then
+  info "authorized_keys are required, but were not provided - the above user will not be created."
+else 
+  useradd -s $${user_shell} -c "$${user_gecos}" -m $${user_login}
+  [ "$${user_supplemental_groups}x" != "x" ] && usermod -G $${user_supplemental_groups} $${user_login}
+  info "Populating authorized_keys for $${user_login}"
+  mkdir ~$${user_login}/.ssh
+  # The ssh_keys variable is put in single-quotes because it may contain it's own double-quotes.
+  printf '$${user_authorized_keys}' > ~$${user_login}/.ssh/authorized_keys
+  chown -R $${user_login}:$${user_login} ~$${user_login}/.ssh
+  chmod -R go= ~$${user_login}/.ssh
+fi
+EOF
 }
 
 locals {
