f7a2cd271a3bd69d1b9c0c65f7df925e29cc13f6
aleksandrandreichenko <andreichenko@web.de>
2022-01-17 16:58:00 +0300

    additional user for bastion

--- gcp/additional_external_user.tf
+++ gcp/additional_external_user.tf
@@ -1,3 +1,11 @@
+data "template_file" "additional_external_user" {
+  count = length(var.additional_external_users)
+
+  vars {
+    user_login 
+  }
+}
+
 locals {
   additional-external-users-script-content = format("%s%s", "#!/bin/bash \n\n", join("\n", data.template_file.additional_external_user.*.rendered))
   additional-external-users-script-md5     = md5(local.additional-external-users-script-content)
