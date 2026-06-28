03923b32b801578239457df3f8d3fadf43fd314f
andreichenko <andreichenko@web.de>
2026-06-28 14:25:13 +0300

    Fix AWS module: add missing variables and pass additional_user_data_end to template

--- aws/variables.tf
+++ aws/variables.tf
@@ -20,10 +20,98 @@ variable "unattended_upgrade_reboot_time" {
 }
 
 variable "unattended_upgrade_email_recipient" {
-  description = "An email address where unattended upgrade errors should be emailed. THis sets the option in /etc/apt/apt.conf.d/50unattended-upgrades"
+  description = "An email address where unattended upgrade errors should be emailed. This sets the option in /etc/apt/apt.conf.d/50unattended-upgrades"
 }
 
 variable "unattended_upgrade_additional_configs" {
   description = "Additional configuration lines to add to /etc/apt/apt.conf.d/50unattended-upgrades"
   default     = ""
+}
+
+variable "vpc_subnet_ids" {
+  description = "A list of VPC subnet IDs where the Auto Scaling Group can launch the bastion instance."
+  type        = list(string)
+}
+
+variable "ssh_public_key_file" {
+  description = "The path or content of the SSH public key file to associate with the default user."
+  type        = string
+}
+
+variable "log_retention" {
+  description = "The number of days to retain log events in the CloudWatch Log Group."
+  type        = number
+  default     = 30
+}
+
+variable "route53_zone_id" {
+  description = "The Route53 Zone ID where the DNS record for the bastion will be created. If left blank, DNS registration is skipped."
+  type        = string
+  default     = ""
+}
+
+variable "remove_root_access" {
+  description = "Whether to remove sudo access from the default ubuntu user. Set to 'true' to remove, or 'false' to retain."
+  type        = string
+  default     = "true"
+}
+
+variable "additional_user_data" {
+  description = "Additional user data shell commands to run before users are created."
+  type        = string
+  default     = ""
+}
+
+variable "additional_user_data_end" {
+  description = "Additional user data shell commands to run after users are created."
+  type        = string
+  default     = ""
+}
+
+variable "additional_users" {
+  description = "List of maps describing additional users to create on the bastion. Keys: login, authorized_keys, shell, supplemental_groups, gecos."
+  type        = list(any)
+  default     = []
+}
+
+variable "additional_external_users" {
+  description = "List of maps describing additional external users to create on the bastion via a separate script. Keys: login, authorized_keys, shell, supplemental_groups, gecos."
+  type        = list(any)
+  default     = []
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type to use for the bastion."
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "encrypt_root_volume" {
+  description = "Whether to encrypt the root block device of the bastion instance."
+  type        = bool
+  default     = true
+}
+
+variable "arn_prefix" {
+  description = "The AWS ARN prefix (e.g. 'arn:aws' or 'arn:aws-us-gov' for GovCloud)."
+  type        = string
+  default     = "arn:aws"
+}
+
+variable "ami_owner_id" {
+  description = "The owner ID of the AMI to use for the standard AWS regions (defaults to Canonical)."
+  type        = string
+  default     = "099720109477"
+}
+
+variable "ami_owner_id_govcloud" {
+  description = "The owner ID of the AMI to use for the GovCloud regions (defaults to Canonical GovCloud)."
+  type        = string
+  default     = "965251226590"
+}
+
+variable "ami_filter_value" {
+  description = "The filter value to search for the base Ubuntu AMI."
+  type        = string
+  default     = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"
 }
